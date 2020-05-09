db72f35e754f5edb8693355938facfb7f8660361
Toni  Diep <adiep123@gmail.com>
2020-05-09 03:55:02 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,19 +8,22 @@ attr_accessor :name, :roster
   end  
   
   def add_student(name, grade)
-    if @roster.has_key?(grade)
-      @roster[grade] << name
-    else
-      @roster[grade] = [name]
-    end
-    # @roster.include?[name]
-    roster
+    # if @roster.has_key?(grade)
+    #   @roster[grade] << name
+    # else
+    #   @roster[grade] = [name]
+    # end
+    
+    @roster.include?[name]
+    roster[grade] =name
+    
     # @roster[grade] ||= []
     # if roster[grade] << name
-    self.roster[grade] ||= []
-    # if self.roster[grade] already exist, leave it alone,
-    # else set self.roster[grade] = []
-    self.roster[grade] << name
+    
+    # self.roster[grade] ||= []
+    # # if self.roster[grade] already exist, leave it alone,
+    # # else set self.roster[grade] = []
+    # self.roster[grade] << name
     
   end
   
