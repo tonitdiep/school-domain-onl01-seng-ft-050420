4dc0af4502b9d88539bd832e4bce84c1f8839039
Toni  Diep <adiep123@gmail.com>
2020-05-09 04:53:53 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -31,8 +31,8 @@ attr_accessor :name, :roster
   end
   
   def sort
-    self.roster.each do |grade, students|
-      self.roster[grade] = students.sort! 
+    @roster.each do |grade, students|
+      @roster[grade] = students.sort! 
       
     # self.roster.each do |grade, students|
     #   students.sort!
